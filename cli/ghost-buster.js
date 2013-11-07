e6ef179efa7e7a473facebc9b29d26eee294dc06
Taron Foxworth <taronfoxworth@gmail.com>
2013-11-07 00:22:08 -0500

    Initial commit

---
+++ cli/ghost-buster.js
@@ -0,0 +1,21 @@
+var program = require('commander'),
+    ghostBuster = require('../lib/ghost-buster'),
+    path = require('path');
+
+program
+  .version('0.0.1')
+  .parse(process.argv);
+
+
+ghostBuster.on('progress', function(msg) {
+  console.log(msg);
+});
+
+ghostBuster.convert(function(err) {
+  if(err) {
+    console.log('ERROR: ' + err);
+  }
+  else {
+    console.log('Ghost Busted.');
+  }
+});
