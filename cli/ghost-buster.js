d74cd0220df7e94284581a15a06395a1b7404736
Taron Foxworth <taronfoxworth@gmail.com>
2013-12-26 14:17:32 -0600

    Added support for deployment of local blog

--- cli/ghost-buster.js
+++ cli/ghost-buster.js
@@ -1,21 +1,50 @@
 var program = require('commander'),
     ghostBuster = require('../lib/ghost-buster'),
-    path = require('path');
+    path = require('path'),
+    fs = require('fs');
 
 program
   .version('0.0.5')
+  .option('-l, --local [path]', 'Pushing Local blog at [path]instead of syncing with cloud storage')
   .parse(process.argv);
 
 
 ghostBuster.on('progress', function(msg) {
+
   console.log(msg);
+
 });
 
-ghostBuster.convert(function(err) {
+if(program.local){
+
+  console.log(program.local);
+
+  if(!fs.existsSync(program.local)){
+
+    //TODO: Check to make sure the local file is inside of pwd
+
+    console.log("Local should be a file");
+
+    return;
+
+  }
+
+}
+
+
+
+ghostBuster.convert(program.local, function(err) {
+
   if(err) {
+  
     console.log('ERROR: ' + err);
+  
   }
+  
   else {
+  
     console.log('Ghost Busted.');
+  
   }
+
 });
