5d4b12e17305ca218006a736496326ce5679bbd0
Taron Foxworth <taronfoxworth@gmail.com>
2014-01-16 15:01:04 -0500

    Created ghost_local, ghost_version, removed unes. console.log, identified ghost version

--- cli/ghost-buster.js
+++ cli/ghost-buster.js
@@ -1,11 +1,13 @@
 var program = require('commander'),
     ghostBuster = require('../lib/ghost-buster'),
     path = require('path'),
-    fs = require('fs');
+    fs = require('fs'),
+    cwd = process.cwd();
 
 program
   .version('0.0.7')
   .option('-l, --local [pathtodb]', 'Pushing Local blog at [path]instead of syncing with cloud storage')
+  .option('-g, --ghostversion [ghostversion]', 'Version of Ghost (Otherwise useing version from package.json)')
   .parse(process.argv);
 
 
@@ -15,15 +17,17 @@ ghostBuster.on('progress', function(msg) {
 
 });
 
+process.env.GHOST_LOCAL = 0;
+
 if(program.local){
 
-  console.log(program.local);
+  process.env.GHOST_LOCAL = 1;
 
   if(!fs.existsSync(program.local)){
 
     //TODO: Check to make sure the local file is inside of pwd
 
-    console.log("Local should be a file");
+    console.log("Please Include a path to a DB.");
 
     return;
 
@@ -31,6 +35,15 @@ if(program.local){
 
 }
 
+if(!program.ghostversion){
+
+  var meta = require(path.join(cwd, 'package.json'));
+
+  program.ghostversion = meta.version;
+
+}
+
+process.env.GHOST_VERSION = program.ghostversion;
 
 
 ghostBuster.convert(program.local, function(err) {
