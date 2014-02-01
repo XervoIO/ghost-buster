6ba65c1c7dc041008dd9ac1149ffc75db23daa8b
Taron Foxworth <taronfoxworth@gmail.com>
2014-02-01 14:32:59 -0500

    Adding support for 4.0

--- cli/ghost-buster.js
+++ cli/ghost-buster.js
@@ -1,13 +1,14 @@
 var program = require('commander'),
-    ghostBuster = require('../lib/ghost-buster'),
-    path = require('path'),
-    fs = require('fs'),
-    cwd = process.cwd();
+  ghostBuster = require('../lib/ghost-buster'),
+  path = require('path'),
+  fs = require('fs'),
+  cwd = process.cwd();
 
 program
   .version('0.0.7')
-  .option('-l, --local [pathtodb]', 'Pushing Local blog at [path]instead of syncing with cloud storage')
+  .option('-db, --database [pathtodb]', 'Pushing Local blog at [path]instead of syncing with cloud storage')
   .option('-g, --ghostversion [ghostversion]', 'Version of Ghost (Otherwise useing version from package.json)')
+  .option('-u, --upgrade', "Upgrade Ghost (Doesn't automatically run ghost-buster)")
   .parse(process.argv);
 
 
@@ -17,47 +18,55 @@ ghostBuster.on('progress', function(msg) {
 
 });
 
-process.env.GHOST_LOCAL = 0;
 
-if(program.local){
+if (!program.upgrade) {
 
-  process.env.GHOST_LOCAL = 1;
+  if (program.db) {
 
-  if(!fs.existsSync(program.local)){
-
-    //TODO: Check to make sure the local file is inside of pwd
-
-    console.log("Please Include a path to a DB.");
-
-    return;
+    program.db = path.join(cwd, "content", "data", "ghost.db");
 
   }
+
+  if (!program.ghostversion) {
+
+    var meta = require(path.join(cwd, 'package.json'));
+
+    program.ghostversion = meta.version;
+
+  }
+
+
+  ghostBuster.convert(program.local, program.ghostversion, function(err) {
+
+    if (err) {
+
+      console.log('ERROR: ' + err);
+
+    } else {
+
+      console.log('Ghost Busted.');
+
+    }
+
+  });
+
+} else {
+
+  console.log('Upgrading...');
+
+  ghostBuster.upgrade(function(err) {
+
+    if (err) {
+
+      console.log('ERROR: ' + err);
+
+    } else {
+
+      console.log('Ghost Successfully upgrade. Please run ghost-buster.');
+
+    }
+
+  });
+
 
 }
-
-if(!program.ghostversion){
-
-  var meta = require(path.join(cwd, 'package.json'));
-
-  program.ghostversion = meta.version;
-
-}
-
-process.env.GHOST_VERSION = program.ghostversion;
-
-
-ghostBuster.convert(program.local, function(err) {
-
-  if(err) {
-  
-    console.log('ERROR: ' + err);
-  
-  }
-  
-  else {
-  
-    console.log('Ghost Busted.');
-  
-  }
-
-});
